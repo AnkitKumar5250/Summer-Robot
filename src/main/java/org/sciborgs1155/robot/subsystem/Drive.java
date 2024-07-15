b17298a81b60b75a31528894ffe8fd3b826adc75
Akk5250 <Akkumar52505250@gmail.com>
2024-07-15 08:17:13 -0400

    Added SwerveDrive and Intake classes(+ some fields)

--- src/main/java/org/sciborgs1155/robot/subsystem/Drive.java
+++
@@ -1,7 +0,0 @@
-package org.sciborgs1155.robot.subsystem;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Drive extends SubsystemBase {
-    
-}
--- src/main/java/org/sciborgs1155/robot/subsystem/Intake.java
+++ src/main/java/org/sciborgs1155/robot/subsystem/Intake.java
@@ -2,6 +2,9 @@ package org.sciborgs1155.robot.subsystem;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake extends SubsystemBase{
+public class Intake extends SubsystemBase {
     
+    public Intake() {
+
+    }
 }
