78d8d2dadf286420d74dfd7e67c6f6c3cb8b0774
Prasad Biswas <prasad3943@gmail.com>
2024-07-25 21:31:56 -0400

    Converted Claw methods to commands

--- src/main/java/org/sciborgs1155/robot/claw/Claw.java
+++ src/main/java/org/sciborgs1155/robot/claw/Claw.java
@@ -8,35 +8,39 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Claw {
+public class Claw extends SubsystemBase{
+
 
     private final TalonFX motor = new TalonFX(CLAW_MOTOR);
     private final TalonFX rollers = new TalonFX(CLAW_ROLLERS);
     private final PIDController pidController = new PIDController(1, 0, 1);
+    private final SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(1,1);
 
     public Claw(){
     
     }
 
-    public void turnClaw(double degrees){
+    public Command turnClaw(double degrees){
 
-        double currentAngle = motor.getPosition().getValueAsDouble();
-
-        while( currentAngle != degrees ){
-            currentAngle = motor.getPosition().getValueAsDouble();
+        return Commands.run(() -> {{
+            double currentAngle = motor.getPosition().getValueAsDouble();
             motor.setVoltage(pidController.calculate(currentAngle, degrees));
-        }
+        }});
         
     }
 
-    public void startRollers(){
-        rollers.set(1);
+    public Command startRollers(){
+        return Commands.runOnce(() -> rollers.set(1));
     }
 
-    public void stopRollers(){
-        rollers.set(0);
+    public Command stopRollers(){
+        return Commands.runOnce(() -> rollers.set(0));
     }
 
 
